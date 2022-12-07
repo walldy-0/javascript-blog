d6c1d1daedcaeb7b559c18ef7faa828e4b5d255c
Waldemar Szymon <walldy@tlen.pl>
2022-12-07 13:04:25 +0100

    Change script.js

--- js/script.js
+++ js/script.js
@@ -4,25 +4,35 @@
   });
   */
 
-  const titleClickHandler = function(event){
+const titleClickHandler = function(event) {
     console.log('Link was clicked!');
     console.log(event);
-  
+
     /* remove class 'active' from all article links  */
-  
-    /* add class 'active' to the clicked link */
-  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+
+    /* add class 'active' to the clicked link <article class="post active" */
+    const activeArticles = document.querySelectorAll('.post.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
     /* remove class 'active' from all articles */
-  
+
     /* get 'href' attribute from the clicked link */
-  
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+
     /* add class 'active' to the correct article */
-  }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-  }
+}
